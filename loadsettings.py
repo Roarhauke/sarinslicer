acbcd35e21f005f6936f78b0fe4366c404f3054f
Roar H. H. D. <roarhauke@gmail.com>
2024-03-05 20:58:19 +0100

    add loadsettings.py, other shit ;)

---
+++ loadsettings.py
@@ -0,0 +1,12 @@
+import json
+
+if __name__ == '__main__':
+	print("get outta here") # i feel like im high on sarin
+	print("free cookies: https://youtu.be/dQw4w9WgXcQ") # wonder how many people i will rickroll
+else:
+	class CSettings: # define CSettings class
+		x = 5 # junk, just for test
+	print("loadsettings module initializing") # report about initialization
+	def loadSetFromFile(file: str) -> CSettings: # define LSFF function
+		p1 = CSettings() # code that might be usefull
+		return p1 # --::--
