d6b63a612a6560683848ff893f157d9323c57a88
Roar H. H. D. <roarhauke@gmail.com>
2024-03-29 20:33:11 +0100

    a lot: "finish" loadsettings, add test params and add presets folder

--- loadsettings.py
+++ loadsettings.py
@@ -5,14 +5,30 @@ if __name__ == '__main__':
 	print("free cookies: https://youtu.be/dQw4w9WgXcQ") # wonder how many people i will rickroll
 else:
 	print("loadsettings module initializing")
-	class CSettings: # define CSettings class
-		def __init__ (self, volx, voly, volz, speed): # constructer
+	class CSettings: # define CSettings=( CodeSettings ) class
+		def __init__ (self, name, volx, voly, volz, maxspeed): # constructer
+			self.name = name
 			self.volx = volx
 			self.voly = voly
 			self.volz = volz
-			self.speed = speed
-	def loadSetFromFile(filename: str) -> CSettings: # define LSFF function
-		file = open(filename, "r") # load file
-		pars = json.loads(file.read()) # parse da file
-		setts = CSettings() # code that might be usefull
+			self.maxspeed = maxspeed
+	def loadSetFromFile(filename: str): # define LSFF function
+		try:
+			file = open(filename, "r") # load file
+		except:
+			raise Exception("requested file does not exist")
+		try:
+			pars = json.loads(file.read()) # parse da file
+		except:
+			raise Exception("json parse error")
+		file.close()
+		try:
+			name = pars["name"]
+			volx = pars["volx"]
+			voly = pars["voly"]
+			volz = pars["volz"]
+			mspeed = pars["maxspeed"]
+		except:
+			raise Exception("data missing")
+		setts = CSettings(name, volx, voly, volz, mspeed) # code that might be usefull
 		return setts # --::--
