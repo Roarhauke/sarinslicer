429dd6daee36a2e513d41fab909d81d112c06209
Roar H. H. D. <roarhauke@gmail.com>
2024-03-30 11:12:50 +0100

    work on toolgcode

--- .tester.py
+++ .tester.py
@@ -1,7 +1,7 @@
 #temp delete
 
-import loadsettings
-sets = loadsettings.loadSetFromFile("presets/test.json")
+import lib
+sets = lib.loadsettings.loadSetFromFile("presets/test.json")
 print(sets.name)
 print(sets.volx)
 print(sets.voly)
--- toolgcode.py
+++ toolgcode.py
@@ -3,5 +3,16 @@ if __name__ == '__main__':
 else:
     print("toolgcode module initializing")
     def toolPathGcode(paths):
+        gcode = []
+        gline = ""
+        extruder = 0
         for x in paths:
-            print(x)
+            gline = ""
+            if x[1] == 0:
+                gline = gline + "g0"
+            else:
+                gline = gline + "g1"
+                extruder += x[1]
+            gline = gline + "x" + str(x[0][0]) + "y" + str(x[0][1]) + "z" + str(x[0][1]) + "e" + str(extruder)
+            gcode.append(gline)
+        return gcode
